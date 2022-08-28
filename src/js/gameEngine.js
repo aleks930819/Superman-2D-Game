83cccbc185fd66f188e1a4fca1c714ed05c0d289
aleks930819 <107752460+aleks930819@users.noreply.github.com>
2022-08-28 21:26:44 +0300

    edit hasCollision

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -214,7 +214,7 @@ function  detectCollision(objectA,objectB){
     let first =  objectA.getBoundingClientRect();
     let second =  objectB.getBoundingClientRect();
 
-    let hasCollision = !(first.top > second.top || first.bottom < second.top || first.right  < second.left || first.left > second.right);
+    let hasCollision = !(first.top > second.bottom || first.bottom < second.top || first.right < second.left || first.left > second.right)
 
     return hasCollision;
 }
